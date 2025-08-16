851db34c8c3b6002f26083cfec7ff615276c4ad7
Michele Franzan <michele.franzan@rimac-technology.com>
2025-08-16 02:21:55 +0200

    Update rock paper scissors with new game logic according to specs

--- rps/script.js
+++ rps/script.js
@@ -19,35 +19,46 @@ const getHumanChoice = function() {
     return sign;
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
 const playRound = function(humanChoice, computerChoice) {
+    let humanScore = 0;
+    let computerScore = 0;
+
     if (((humanChoice === "rock") && (computerChoice === "paper")) ||
             ((humanChoice === "paper") && (computerChoice === "scissors")) ||
             ((humanChoice === "scissors") && (computerChoice === "rock"))) {
         computerScore++;
-        alert(`You: ${humanChoice} - Computer: ${computerChoice}\nComputer won 😢\nYou: ${humanScore} - Computer: ${computerScore}`);
+        alert(`You: ${humanChoice} - Computer: ${computerChoice}\nComputer won 😢`);
     } else if (((humanChoice === "rock") && (computerChoice === "scissors")) ||
             ((humanChoice === "paper") && (computerChoice === "rock")) ||
             ((humanChoice === "scissors") && (computerChoice === "paper"))) {
         humanScore++;
-        alert(`You: ${humanChoice} - Computer: ${computerChoice}\nYou won 😄\nYou: ${humanScore} - Computer: ${computerScore}`);
+        alert(`You: ${humanChoice} - Computer: ${computerChoice}\nYou won 😄`);
     } else {
-        alert(`You: ${humanChoice} - Computer: ${computerChoice}\nThat's a tie!\nYou: ${humanScore} - Computer: ${computerScore}`);
+        alert(`You: ${humanChoice} - Computer: ${computerChoice}\nThat's a tie!`);
+    }
+
+    return [humanScore, computerScore]
+}
+
+const playGame = function() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        result = playRound(humanSelection, computerSelection);
+        humanScore += result[0];
+        computerScore += result[1];
+    }
+
+    if(humanScore > computerScore) {
+        alert(`Final result: You: ${humanScore} - Computer: ${computerScore}\nCongratulation, you won 🥳`);
+    } else if (computerScore > humanScore) {
+        alert(`Final result: You: ${humanScore} - Computer: ${computerScore}\nTry again next time 😵`);
+    } else {
+        alert(`Final result: You: ${humanScore} - Computer: ${computerScore}\nPerfectly balanced, as all things should be!`);
     }
 }
 
-for(let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-}
-
-if(humanScore > computerScore) {
-    alert(`Final result: You: ${humanScore} - Computer: ${computerScore}\nCongratulation, you won 🥳`);
-} else if (computerScore > humanScore) {
-    alert(`Final result: You: ${humanScore} - Computer: ${computerScore}\nTry again next time 😵`);
-} else {
-    alert(`Final result: You: ${humanScore} - Computer: ${computerScore}\nPerfectly balanced, as all things should be!`);
-}
+playGame();
